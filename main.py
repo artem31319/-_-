bbd5ce6264006d3dc19a6702b75e64008a626131
artem31319 <128899465+artem31319@users.noreply.github.com>
2023-03-25 13:44:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print("_|_|_\n_|_|_\n  |  |  ")
+pervoe = int(input("1,2,3,4,5,6,7,8 или 9?"))
+if pervoe == 1:
+    print("_+|__|__\n__|__|__\n  |  |  ")
+if vtoroe == 2:
+    print("__||__\n__|__|__\n  |  |  ")
